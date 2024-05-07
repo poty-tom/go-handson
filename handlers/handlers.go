0f43c9318eb1d5677481cd230ea4cecf4936f247
potechomp <m.takaku.1223@gmail.com>
2024-05-07 12:21:35 +0000

    json encode/decode

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
+
+	"github.com/poty-tom/go-handson/models"
 )
 
 func HelthCheck(w http.ResponseWriter, req *http.Request) {
@@ -13,7 +16,6 @@ func HelthCheck(w http.ResponseWriter, req *http.Request) {
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
-
 	var page int
 	if p, ok := queryMap["page"]; ok && len(p) > 0 {
 		var err error
@@ -28,4 +30,16 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 
 	resStr := fmt.Sprintf("Article List (page %d)\n", page)
 	io.WriteString(w, resStr)
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	article := reqArticle
+
+	json.NewEncoder(w).Encode(article)
+
 }
--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.HelthCheck)
-	r.HandleFunc("/article", handlers.ArticleListHandler).Queries("page", "{page}")
+	r.HandleFunc("/", handlers.HelthCheck).Methods(http.MethodGet)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
