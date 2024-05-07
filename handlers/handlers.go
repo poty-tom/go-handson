0b416e52eab2b1f3948b0c8ffd6e5f0bb7548151
potechomp <m.takaku.1223@gmail.com>
2024-05-07 12:47:00 +0000

    handlerの改善と追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,20 +2,24 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/poty-tom/go-handson/models"
 )
 
+// [GET: /]
+// ヘルスチェック用
 func HelthCheck(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello world")
 }
 
+// [GET: /article/list/page={number}]
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	queryMap := req.URL.Query()
+	queryMap := req.URL.Query() //この時点でクエリパラメータをマップに格納
 	var page int
 	if p, ok := queryMap["page"]; ok && len(p) > 0 {
 		var err error
@@ -28,10 +32,28 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 		page = 1
 	}
 
-	resStr := fmt.Sprintf("Article List (page %d)\n", page)
-	io.WriteString(w, resStr)
+	log.Println(page)
+	articleList := []models.Article{models.Article1}
+	json.NewEncoder(w).Encode(articleList)
 }
 
+// [GET: /article/{id}]
+// {id}値に応じたArticleデータ取得
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"]) // パスパラメータを取得
+	if err != nil {
+		http.Error(w, "invalid path parameter", http.StatusBadRequest)
+		return
+	}
+
+	// TODO: ArticleIDを検索
+	log.Println(articleID)
+	article := models.Article1
+	json.NewEncoder(w).Encode(article)
+}
+
+// [POST: /article]
+// Articleデータ作成
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
@@ -42,4 +64,27 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 	json.NewEncoder(w).Encode(article)
 
+}
+
+// [POST: /article/nice]
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	// TODO: ArticleにNiceする
+	article := reqArticle
+	json.NewEncoder(w).Encode(&article)
+}
+
+// [POST: /comment]
+// commentデータ作成
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+
+	comment := reqComment
+	json.NewEncoder(w).Encode(comment)
 }
--- main.go
+++ main.go
@@ -13,7 +13,9 @@ func main() {
 	r.HandleFunc("/", handlers.HelthCheck).Methods(http.MethodGet)
 	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
 	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-
+	r.HandleFunc("/artcile/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/coment", handlers.PostCommentHandler).Methods(http.MethodPost)
 	log.Println("server start at port: 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
